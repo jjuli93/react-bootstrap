5b729936a261bfde4fa5cbc6451896f677dbb536
Julian alvarez <alvarezjulianignacio@gmail.com>
2020-09-03 00:28:31 -0300

    delete

--- src/app/components/Spinner/index.tsx
+++ src/app/components/Spinner/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { SpinnerProps } from 'react-spinkit';
 
 import Loading from './components/loading';
 
 interface Props extends SpinnerProps {
   loading: boolean;
+  children: ReactNode;
 }
 
-export const withSpinner = ({ loading, ...spinnerProps }: Props) =>
-  loading ? <Loading {...spinnerProps} /> : <p>not loading</p>;
+export const spinnerContainer = ({ loading, children, ...spinnerProps }: Props) =>
+  loading ? <Loading {...spinnerProps} /> : { children };
